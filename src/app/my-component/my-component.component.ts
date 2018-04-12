797135014bed216bac87e3921d3ff859de4101a6
hari9513 <harikrishna9513@gmail.com>
2018-04-13 01:25:50 +0530

    Crated table with child color

--- src/app/my-component/my-component.component.ts
+++ src/app/my-component/my-component.component.ts
@@ -9,10 +9,12 @@ export class MyComponentComponent implements OnInit {
 first:string
 second:string
 third:string
+isShow:boolean
 
   constructor() { }
 
   ngOnInit() {
+  this.isShow = true;
   }
 orange(){
 if(this.first === 'Orange'){
@@ -36,4 +38,105 @@ return;
 this.third = 'Green';
 }
 
+flightDetails = [
+{
+"id": 1,
+"name": "Jet Airways",
+"bordingPoint": "Chennai",
+"destinationPoint": "Bangalore",
+"departureTime": "2018-04-11T01:58:13.768Z",
+"arrivalTime": "2018-04-11T02:58:13.768Z"
+},
+{
+"id": 2,
+"name": "Air India",
+"bordingPoint": "Mumbai",
+"destinationPoint": "Bangalore",
+"departureTime": "2018-04-12T01:58:13.768Z",
+"arrivalTime": "2018-04-12T02:58:13.768Z"
+},
+{
+"id": 3,
+"name": "IndiGo",
+"bordingPoint": "Bangalore",
+"destinationPoint": "Kolkata",
+"departureTime": "2018-04-15T01:58:13.768Z",
+"arrivalTime": "2018-04-15T02:58:13.768Z"
+},
+{
+"id": 4,
+"name": "SpiceJet",
+"bordingPoint": "Delhi",
+"destinationPoint": "Bangalore",
+"departureTime": "2018-04-14T01:58:13.768Z",
+"arrivalTime": "2018-04-14T02:58:13.768Z"
+},
+{
+"id": 5,
+"name": "GoAir",
+"bordingPoint": "Kolkata",
+"destinationPoint": "Chennai",
+"departureTime": "2018-04-13T01:58:13.768Z",
+"arrivalTime": "2018-04-13T02:58:13.768Z"
+},
+{
+"id": 6,
+"name": "Vistara",
+"bordingPoint": "Chennai",
+"destinationPoint": "Bangalore",
+"departureTime": "2018-04-17T01:58:13.768Z",
+"arrivalTime": "2018-04-17T02:58:13.768Z"
+},
+{
+"id": 7,
+"name": "Zoom Air",
+"bordingPoint": "Kolkata",
+"destinationPoint": "Bangalore",
+"departureTime": "2018-04-19T01:58:13.768Z",
+"arrivalTime": "2018-04-19T02:58:13.768Z"
+},
+{
+"id": 8,
+"name": "Blue Dart Aviation",
+"bordingPoint": "Chennai",
+"destinationPoint": "Mumbai",
+"departureTime": "2018-04-13T01:58:13.768Z",
+"arrivalTime": "2018-04-13T02:58:13.768Z"
+},
+{
+"id": 9,
+"name": "Deccan Charters",
+"bordingPoint": "Chennai",
+"destinationPoint": "Bangalore",
+"departureTime": "2018-04-17T01:58:13.768Z",
+"arrivalTime": "2018-04-17T02:58:13.768Z"
+},
+{
+"id": 10,
+"name": "TajAir",
+"bordingPoint": "Chennai",
+"destinationPoint": "Delhi",
+"departureTime": "2018-04-15T01:58:13.768Z",
+"arrivalTime": "2018-04-15T02:58:13.768Z"
+},
+{
+"id": 11,
+"name": "Titan Aviation",
+"bordingPoint": "Chennai",
+"destinationPoint": "Bangalore",
+"departureTime": "2018-04-11T01:58:13.768Z",
+"arrivalTime": "2018-04-11T02:58:13.768Z"
 }
+];
+
+onClick(){
+	if(this.isShow == true){
+	this.isShow = false;
+	return;
+	}
+
+	this.isShow = true;
+	
+}
+
+};
